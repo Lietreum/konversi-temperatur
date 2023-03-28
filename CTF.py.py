6b3e5ad7c26f8ec2f6d0613390a5de1b95d936af
Lietreum <127808721+Lietreum@users.noreply.github.com>
2023-03-28 09:42:50 +0700

    Add files via upload

---
+++ CTF.py.py
@@ -0,0 +1,9 @@
+# [Absen 31]
+
+# Meminta pengguna untuk memasukkan input nilai
+celcius = float(input("Masukkan suhu dalam derajat celcius = "))
+# Menghitung suhu Fahreheit 
+fahrenheit = (celcius * 9/5) + 32
+# Untuk mencetak output
+print(f"{celcius} derajat Celcius = {fahrenheit:.1f} derajat Fahrenheit")
+# 1.f untuk menformat suhu yang sudah dikonversi hanya satu angka desimal
